778ede925ff7e58df78f5310c016545e7a6a6882
Matisse Mallette <matissemallette@gmail.com>
2023-03-07 15:04:40 -0700

    Actually implement tests.

--- spec/facades/air_quality_facade_spec.rb
+++ spec/facades/air_quality_facade_spec.rb
@@ -11,6 +11,14 @@ RSpec.describe 'Air Quality Facade' do
     json_response = JSON.parse(response, symbolize_names: true)
 
     air_quality = AirQualityFacade.find_air_quality('19.43', '-99.13')
+
+    expect(air_quality.id).to eq(nil)
+    expect(air_quality.aqi).to eq(4)
+    expect(air_quality.aqi).to be_an(Integer)
+    expect(air_quality.datetime).to eq(1678224114)
+    expect(air_quality.datetime).to be_an(Integer)
+    expect(air_quality.readable_aqi).to eq('Poor')
+    expect(air_quality.readable_aqi).to be_a(String)
   end
   it 'can convert readable_aqi integer into string' do
     readable_air_quality_string = AirQualityFacade.readable_aqi[1]
