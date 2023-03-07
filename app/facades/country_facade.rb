a0ee6c832421ece38a6671d74129a0eb3a62a9fc
Matisse Mallette <matissemallette@gmail.com>
2023-03-07 09:04:51 -0700

    Create country service.

--- app/facades/country_facade.rb
+++ app/facades/country_facade.rb
@@ -1,8 +1,13 @@
 class CountryFacade
-  def self.random_country
+  def self.random_country_name
     countries = CountryService.countries.map do |country|
       country[:name][:common]
     end
     countries.shuffle.sample
+  end
+
+  def self.find_country(name)
+    data = CountryService.find_country(name).first
+    Country.new(data)
   end
 end
